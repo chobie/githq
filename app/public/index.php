87e4c9f79f7d722b55ff11bbc1bc1b88ac23aea3
Shuhei Tanuma <stanuma@zynga.com>
2011-11-28 12:52:05 +0900

    added stored config

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . "/../vendor/php-uikit/UIKit/Framework/UIAutoLoader.php";
+require __DIR__ . "/../config/development.php";
 
 UIKit\Framework\UIAutoLoader::register();
 UIKit\Framework\UIAutoLoader::add_include_path(dirname(__DIR__) . '/libs');
