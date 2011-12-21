aaae1373293a8888a7077799c068fd514b7022b2
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2011-12-22 02:02:18 +0900

    for now, move db conf

--- app/public/index.php
+++ app/public/index.php
@@ -2,43 +2,7 @@
 require __DIR__ . '/../config/env.php';
 
 class GitHQApplicationDelegate extends UIKit\Framework\UIWebApplicationDelegate
-{
-	public function didBecomeActive()
-	{
-		$data = file_get_contents(__DIR__ . "/../config/entities.xml");
-		$xml = simplexml_load_string($data);
-		
-		$result = array();
-		foreach($xml->xpath("//entity") as $element) {
-			$id = (string)$element->attributes()->id;
-			$result[$id]['strategy']      = (string)$element->strategy;
-			$result[$id]['serializer']   = (string)$element->serializer;
-			$result[$id]['cache']        = (string)$element->cache;
-			$result[$id]['expiration']   = (string)$element->expiration;
-			$result[$id]['lock_timeout'] = (string)$element->lock_timeout;
-
-			foreach ($element->redis as $redis) {
-				$result[$id]['redis'] = array(
-					"host" => (string)$redis->host,
-					"port" => (string)$redis->port,
-					"persistence" => (string)$redis->persistence,
-				);
-			}
-		}
-		
-		$conf= UIKit\Framework\UIStoredConfig::getInstance();
-		foreach($result as $key => $config) {
-			$conf->set($key,$config);
-		}
-
-		$i = UIKit\Framework\UIStoredUnderlying::getInstance();
-		foreach ($conf->keys() as $key) {
-			$i->addStrategy($key, $conf->get($key . ".strategy"));
-			$i->addSerializer($key, $conf->get($key . ".serializer"));
-			$i->addCache($key,$conf->get($key . '.cache'));
-		}
-	}
-	
+{	
 	protected function getRouting()
 	{
 		$router = new UIKit\Framework\UIRouter();
