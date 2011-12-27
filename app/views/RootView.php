b04ac6abd7bdf08eb0895a6a2197bfa8df0d6910
Shuhei Tanuma <stanuma@zynga.com>
2011-12-27 13:43:10 +0900

    forget to commit files

---
+++ app/views/RootView.php
@@ -0,0 +1,46 @@
+<?php
+
+class RootView
+{
+	protected $use_layout = true;
+	protected $template;
+	protected $container;
+	protected $user;
+	
+	public function __construct($container)
+	{
+		$this->container = $container;
+		$this->render = new UIKit\Framework\Render\Twig($container);
+
+		$this->viewDidLoad();
+	}
+	
+	public function setUser($user)
+	{
+		$this->user = $user;
+	}
+	
+	public function viewDidLoad()
+	{
+		if (!$this->template) {
+			$this->template = "index.htm";
+		}
+	}
+	
+	public function setTemplate($file)
+	{
+		$this->template = $file;
+		return $this;
+	}
+	
+	public function prepareResponse($vars = array())
+	{
+		$vars['user'] = $this->user;
+		$organizations = null;
+		if ($this->user) {
+			$vars['user'] = $this->user->getJoinedOrganizations();
+		}
+		
+		return new UIKit\Framework\HTTPFoundation\Response\HTTPResponse($this->render->render2($this->template, $vars));
+	}
+}
